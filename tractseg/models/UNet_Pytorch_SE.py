3cef327cd4a0ff669fe2d2d58fb20cf1111e494f
wasserth <j.wasserthal@dkfz.de>
2017-11-16 18:29:35 +0100

    Add SE Net

--- tractseg/models/UNet_Pytorch_SE.py
+++ tractseg/models/UNet_Pytorch_SE.py
@@ -26,6 +26,26 @@ from tractseg.libs.PytorchUtils import PytorchUtils
 from tractseg.libs.ExpUtils import ExpUtils
 from tractseg.models.BaseModel import BaseModel
 
+# from torchvision.models import ResNet
+
+# channel -> same as in_channel conv
+# reduction -> number of neurons in FC (?)
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=16):  #reduction=16
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+                nn.Linear(channel, reduction),
+                nn.ReLU(inplace=True),
+                nn.Linear(reduction, channel),
+                nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y
 
 def conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=True, batchnorm=False):
     if batchnorm:
@@ -47,6 +67,120 @@ def deconv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=0, outp
         nn.ReLU())
     return layer
 
+class UNet_Skip(torch.nn.Module):
+    def __init__(self, n_input_channels=3, n_classes=7, n_filt=64):
+        super(UNet_Skip, self).__init__()
+        self.in_channel = n_input_channels
+        self.n_classes = n_classes
+
+        self.contr_1_1 = conv2d(n_input_channels, n_filt)
+        self.contr_1_2 = conv2d(n_filt, n_filt)
+        self.se_c1 = SELayer(n_filt)
+        self.pool_1 = nn.MaxPool2d((2, 2))
+
+        self.contr_2_1 = conv2d(n_filt + n_filt, n_filt * 2)
+        self.contr_2_2 = conv2d(n_filt * 2, n_filt * 2)
+        self.se_c2 = SELayer(n_filt * 2)
+        self.pool_2 = nn.MaxPool2d((2, 2))
+
+        self.contr_3_1 = conv2d(n_filt*2 + n_filt * 2, n_filt * 4)
+        self.contr_3_2 = conv2d(n_filt * 4, n_filt * 4)
+        self.se_c3 = SELayer(n_filt * 4)
+        self.pool_3 = nn.MaxPool2d((2, 2))
+
+        self.contr_4_1 = conv2d(n_filt*4 + n_filt * 4, n_filt * 8)
+        self.contr_4_2 = conv2d(n_filt * 8, n_filt * 8)
+        self.se_c4 = SELayer(n_filt * 8)
+        self.pool_4 = nn.MaxPool2d((2, 2))
+
+        self.dropout = nn.Dropout(p=0.4)
+
+        self.encode_1 = conv2d(n_filt*8 + n_filt * 8, n_filt * 16)
+        self.encode_2 = conv2d(n_filt * 16, n_filt * 16)
+        self.se_e1 = SELayer(n_filt * 16)
+        self.deconv_1 = deconv2d(n_filt*16 + n_filt * 16, n_filt * 16, kernel_size=2, stride=2)
+        # self.deconv_1 = nn.Upsample(scale_factor=2)     #does only upscale width and height  #Similar results to deconv2d
+
+        self.expand_1_1 = conv2d(n_filt * 8 + n_filt * 16, n_filt * 8)
+        self.expand_1_2 = conv2d(n_filt * 8, n_filt * 8)
+        # self.se_e2 = SELayer(n_filt * 8)
+        self.deconv_2 = deconv2d(n_filt * 8, n_filt * 8, kernel_size=2, stride=2)
+        # self.deconv_2 = nn.Upsample(scale_factor=2)
+
+        self.expand_2_1 = conv2d(n_filt * 4 + n_filt * 8, n_filt * 4, stride=1)
+        self.expand_2_2 = conv2d(n_filt * 4, n_filt * 4, stride=1)
+        # self.se_e3 = SELayer(n_filt * 4)
+        self.deconv_3 = deconv2d(n_filt * 4, n_filt * 4, kernel_size=2, stride=2)
+        # self.deconv_3 = nn.Upsample(scale_factor=2)
+
+        self.expand_3_1 = conv2d(n_filt * 2 + n_filt * 4, n_filt * 2, stride=1)
+        self.expand_3_2 = conv2d(n_filt * 2, n_filt * 2, stride=1)
+        # self.se_e4 = SELayer(n_filt * 2)
+        self.deconv_4 = deconv2d(n_filt * 2, n_filt * 2, kernel_size=2, stride=2)
+        # self.deconv_4 = nn.Upsample(scale_factor=2)
+
+        self.expand_4_1 = conv2d(n_filt + n_filt * 2, n_filt, stride=1)
+        self.expand_4_2 = conv2d(n_filt, n_filt, stride=1)
+        # self.se_e5 = SELayer(n_filt)
+
+        self.conv_5 = nn.Conv2d(n_filt, n_classes, kernel_size=1, stride=1, padding=0, bias=True)  # no activation function, because is in LossFunction (...WithLogits)
+
+    def forward(self, inpt):
+        contr_1_1 = self.contr_1_1(inpt)
+        contr_1_2 = self.contr_1_2(contr_1_1)
+        contr_1_2 = self.se_c1(contr_1_2)
+        pool_1 = self.pool_1(torch.cat([contr_1_2, contr_1_1], 1))
+
+        contr_2_1 = self.contr_2_1(pool_1)
+        contr_2_2 = self.contr_2_2(contr_2_1)
+        contr_2_2 = self.se_c2(contr_2_2)
+        pool_2 = self.pool_2(torch.cat([contr_2_2, pool_1], 1))
+
+        contr_3_1 = self.contr_3_1(pool_2)
+        contr_3_2 = self.contr_3_2(contr_3_1)
+        contr_3_2 = self.se_c3(contr_3_2)
+        pool_3 = self.pool_3(torch.cat([contr_3_2, pool_2], 1))
+
+        contr_4_1 = self.contr_4_1(pool_3)
+        contr_4_2 = self.contr_4_2(contr_4_1)
+        contr_4_2 = self.se_c4(contr_4_2)
+        pool_4 = self.pool_4(torch.cat([contr_4_2, pool_3], 1))
+
+        dropout = self.dropout(pool_4)
+
+        # encode_1 = self.encode_1(dropout)
+        encode_1 = self.encode_1(pool_4)
+        encode_2 = self.encode_2(encode_1)
+        encode_2 = self.se_e1(encode_2)
+        deconv_1 = self.deconv_1(torch.cat([encode_2, pool_4], 1))
+
+        concat1 = torch.cat([deconv_1, contr_4_2], 1)
+        expand_1_1 = self.expand_1_1(concat1)
+        expand_1_2 = self.expand_1_2(expand_1_1)
+        # expand_1_2 = self.se_e2(expand_1_2)
+        deconv_2 = self.deconv_2(expand_1_2)
+
+        concat2 = torch.cat([deconv_2, contr_3_2], 1)
+        expand_2_1 = self.expand_2_1(concat2)
+        expand_2_2 = self.expand_2_2(expand_2_1)
+        # expand_2_2 = self.se_e3(expand_2_2)
+        deconv_3 = self.deconv_3(expand_2_2)
+
+        concat3 = torch.cat([deconv_3, contr_2_2], 1)
+        expand_3_1 = self.expand_3_1(concat3)
+        expand_3_2 = self.expand_3_2(expand_3_1)
+        # expand_3_2 = self.se_e4(expand_3_2)
+        deconv_4 = self.deconv_4(expand_3_2)
+
+        concat4 = torch.cat([deconv_4, contr_1_2], 1)
+        expand_4_1 = self.expand_4_1(concat4)
+        expand_4_2 = self.expand_4_2(expand_4_1)
+        # expand_4_2 = self.se_e5(expand_4_2)
+
+        conv_5 = self.conv_5(expand_4_2)
+        return conv_5
+
+
 
 class UNet(torch.nn.Module):
     def __init__(self, n_input_channels=3, n_classes=7, n_filt=64):
@@ -56,94 +190,113 @@ class UNet(torch.nn.Module):
 
         self.contr_1_1 = conv2d(n_input_channels, n_filt)
         self.contr_1_2 = conv2d(n_filt, n_filt)
+        self.se_c1 = SELayer(n_filt)
         self.pool_1 = nn.MaxPool2d((2, 2))
 
         self.contr_2_1 = conv2d(n_filt, n_filt * 2)
         self.contr_2_2 = conv2d(n_filt * 2, n_filt * 2)
+        self.se_c2 = SELayer(n_filt * 2)
         self.pool_2 = nn.MaxPool2d((2, 2))
 
         self.contr_3_1 = conv2d(n_filt * 2, n_filt * 4)
         self.contr_3_2 = conv2d(n_filt * 4, n_filt * 4)
+        self.se_c3 = SELayer(n_filt * 4)
         self.pool_3 = nn.MaxPool2d((2, 2))
 
         self.contr_4_1 = conv2d(n_filt * 4, n_filt * 8)
         self.contr_4_2 = conv2d(n_filt * 8, n_filt * 8)
+        self.se_c4 = SELayer(n_filt * 8)
         self.pool_4 = nn.MaxPool2d((2, 2))
 
         self.dropout = nn.Dropout(p=0.4)
 
         self.encode_1 = conv2d(n_filt * 8, n_filt * 16)
         self.encode_2 = conv2d(n_filt * 16, n_filt * 16)
+        self.se_e1 = SELayer(n_filt * 16)
         self.deconv_1 = deconv2d(n_filt * 16, n_filt * 16, kernel_size=2, stride=2)
         # self.deconv_1 = nn.Upsample(scale_factor=2)     #does only upscale width and height  #Similar results to deconv2d
 
         self.expand_1_1 = conv2d(n_filt * 8 + n_filt * 16, n_filt * 8)
         self.expand_1_2 = conv2d(n_filt * 8, n_filt * 8)
+        self.se_e2 = SELayer(n_filt * 8)
         self.deconv_2 = deconv2d(n_filt * 8, n_filt * 8, kernel_size=2, stride=2)
         # self.deconv_2 = nn.Upsample(scale_factor=2)
 
         self.expand_2_1 = conv2d(n_filt * 4 + n_filt * 8, n_filt * 4, stride=1)
         self.expand_2_2 = conv2d(n_filt * 4, n_filt * 4, stride=1)
+        self.se_e3 = SELayer(n_filt * 4)
         self.deconv_3 = deconv2d(n_filt * 4, n_filt * 4, kernel_size=2, stride=2)
         # self.deconv_3 = nn.Upsample(scale_factor=2)
 
         self.expand_3_1 = conv2d(n_filt * 2 + n_filt * 4, n_filt * 2, stride=1)
         self.expand_3_2 = conv2d(n_filt * 2, n_filt * 2, stride=1)
+        self.se_e4 = SELayer(n_filt * 2)
         self.deconv_4 = deconv2d(n_filt * 2, n_filt * 2, kernel_size=2, stride=2)
         # self.deconv_4 = nn.Upsample(scale_factor=2)
 
         self.expand_4_1 = conv2d(n_filt + n_filt * 2, n_filt, stride=1)
         self.expand_4_2 = conv2d(n_filt, n_filt, stride=1)
+        self.se_e5 = SELayer(n_filt)
 
         self.conv_5 = nn.Conv2d(n_filt, n_classes, kernel_size=1, stride=1, padding=0, bias=True)  # no activation function, because is in LossFunction (...WithLogits)
 
     def forward(self, inpt):
         contr_1_1 = self.contr_1_1(inpt)
         contr_1_2 = self.contr_1_2(contr_1_1)
+        contr_1_2 = self.se_c1(contr_1_2)
         pool_1 = self.pool_1(contr_1_2)
 
         contr_2_1 = self.contr_2_1(pool_1)
         contr_2_2 = self.contr_2_2(contr_2_1)
+        contr_2_2 = self.se_c2(contr_2_2)
         pool_2 = self.pool_2(contr_2_2)
 
         contr_3_1 = self.contr_3_1(pool_2)
         contr_3_2 = self.contr_3_2(contr_3_1)
+        contr_3_2 = self.se_c3(contr_3_2)
         pool_3 = self.pool_3(contr_3_2)
 
         contr_4_1 = self.contr_4_1(pool_3)
         contr_4_2 = self.contr_4_2(contr_4_1)
+        contr_4_2 = self.se_c4(contr_4_2)
         pool_4 = self.pool_4(contr_4_2)
 
-        pool_4 = self.dropout(pool_4)
+        dropout = self.dropout(pool_4)
 
+        # encode_1 = self.encode_1(dropout)
         encode_1 = self.encode_1(pool_4)
         encode_2 = self.encode_2(encode_1)
+        encode_2 = self.se_e1(encode_2)
         deconv_1 = self.deconv_1(encode_2)
 
         concat1 = torch.cat([deconv_1, contr_4_2], 1)
         expand_1_1 = self.expand_1_1(concat1)
         expand_1_2 = self.expand_1_2(expand_1_1)
+        expand_1_2 = self.se_e2(expand_1_2)
         deconv_2 = self.deconv_2(expand_1_2)
 
         concat2 = torch.cat([deconv_2, contr_3_2], 1)
         expand_2_1 = self.expand_2_1(concat2)
         expand_2_2 = self.expand_2_2(expand_2_1)
+        expand_2_2 = self.se_e3(expand_2_2)
         deconv_3 = self.deconv_3(expand_2_2)
 
         concat3 = torch.cat([deconv_3, contr_2_2], 1)
         expand_3_1 = self.expand_3_1(concat3)
         expand_3_2 = self.expand_3_2(expand_3_1)
+        expand_3_2 = self.se_e4(expand_3_2)
         deconv_4 = self.deconv_4(expand_3_2)
 
         concat4 = torch.cat([deconv_4, contr_1_2], 1)
         expand_4_1 = self.expand_4_1(concat4)
         expand_4_2 = self.expand_4_2(expand_4_1)
+        expand_4_2 = self.se_e5(expand_4_2)
 
         conv_5 = self.conv_5(expand_4_2)
         return conv_5
 
 
-class UNet_Pytorch(BaseModel):
+class UNet_Pytorch_SE(BaseModel):
     def create_network(self):
 
         def train(X, y):
@@ -216,9 +369,13 @@ class UNet_Pytorch(BaseModel):
             NR_OF_GRADIENTS = 33
 
         if torch.cuda.is_available():
-            net = UNet(n_input_channels=NR_OF_GRADIENTS, n_classes=self.HP.NR_OF_CLASSES, n_filt=self.HP.UNET_NR_FILT).cuda()
+            #todo important: change
+            # net = UNet(n_input_channels=NR_OF_GRADIENTS, n_classes=self.HP.NR_OF_CLASSES, n_filt=self.HP.UNET_NR_FILT).cuda()
+            net = UNet_Skip(n_input_channels=NR_OF_GRADIENTS, n_classes=self.HP.NR_OF_CLASSES, n_filt=self.HP.UNET_NR_FILT).cuda()
         else:
-            net = UNet(n_input_channels=NR_OF_GRADIENTS, n_classes=self.HP.NR_OF_CLASSES, n_filt=self.HP.UNET_NR_FILT)
+            #todo important: change
+            # net = UNet(n_input_channels=NR_OF_GRADIENTS, n_classes=self.HP.NR_OF_CLASSES, n_filt=self.HP.UNET_NR_FILT)
+            net = UNet_Skip(n_input_channels=NR_OF_GRADIENTS, n_classes=self.HP.NR_OF_CLASSES, n_filt=self.HP.UNET_NR_FILT)
         criterion = nn.BCEWithLogitsLoss()
         optimizer = optim.Adamax(net.parameters(), lr=self.HP.LEARNING_RATE)
         # optimizer = optim.Adam(net.parameters(), lr=self.HP.LEARNING_RATE)  #very slow (half speed of Adamax) -> strange
