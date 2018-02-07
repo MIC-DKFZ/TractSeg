03f4e5886336856119bd3c8820e2ad791cd90577
wasserth <j.wasserthal@dkfz.de>
2018-02-07 17:28:56 +0100

    Autofill LABELS_FILENAME

--- tractseg/config/HighResHP.py
+++ tractseg/config/HighResHP.py
@@ -10,6 +10,5 @@ class HP(BaseHP):
 
     DATASET = "HCP"  # HCP / HCP_32g
     RESOLUTION = "1.25mm"  # 1.25mm (/ 2.5mm)
-    FEATURES_FILENAME = "270g_125mm_peaks"  # 12g90g270g / 270g_125mm_xyz / 270g_125mm_peaks / 90g_125mm_peaks / 32g_25mm_peaks / 32g_25mm_xyz
-    LABELS_FILENAME = "bundle_peaks/CA"  # IMPORTANT: Adapt BatchGen if 808080              # bundle_masks / bundle_masks_72 / bundle_masks_dm / bundle_peaks      #Only used when using DataManagerNifti
-    CLASSES = "CA"
+    FEATURES_FILENAME = "12g90g270g"  # 12g90g270g / 270g_125mm_xyz / 270g_125mm_peaks / 90g_125mm_peaks / 32g_25mm_peaks / 32g_25mm_xyz
+    CLASSES = "11"     # All / 11 / CST_right
--- tractseg/config/LowResHP.py
+++ tractseg/config/LowResHP.py
@@ -11,5 +11,4 @@ class HP(BaseHP):
     DATASET = "HCP_32g"  # HCP / HCP_32g
     RESOLUTION = "2.5mm"  # 1.25mm (/ 2.5mm)
     FEATURES_FILENAME = "32g_25mm_peaks"  # 12g90g270g / 270g_125mm_xyz / 270g_125mm_peaks / 90g_125mm_peaks / 32g_25mm_peaks / 32g_25mm_xyz
-    LABELS_FILENAME = "bundle_peaks/CA"  # IMPORTANT: Adapt BatchGen if 808080              # bundle_masks / bundle_masks_72 / bundle_masks_dm / bundle_peaks      #Only used when using DataManagerNifti
-    CLASSES = "11"
+    CLASSES = "11"      # All / 11 / CST_right
