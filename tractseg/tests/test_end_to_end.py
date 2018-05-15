5b35fc4b2f5948067a7d9b6c6eb2dea37f47424b
wasserth <j.wasserthal@dkfz.de>
2018-05-15 18:41:33 +0200

    Add testing of TOMs

--- tractseg/tests/test_end_to_end.py
+++ tractseg/tests/test_end_to_end.py
@@ -10,15 +10,11 @@ class test_end_to_end(unittest.TestCase):
     def test_tractseg_output(self):
         img_ref = nib.load("examples/Tests/bundle_segmentations.nii.gz").get_data()
         img_new = nib.load("examples/tractseg_output/bundle_segmentations.nii.gz").get_data()
-
         images_equal = np.array_equal(img_ref, img_new)
-
         self.assertTrue(images_equal, "Tract segmentations are not correct")
 
-    # def test_peakreg_output(self):
-    #     img_ref = nib.load("examples/example_output_TOM.nii.gz").get_data()
-    #     img_new = nib.load("examples/tractseg_output/bundle_TOMs.nii.gz").get_data()
-    #
-    #     images_equal = np.array_equal(img_ref, img_new)
-    #
-    #     self.assertTrue(images_equal, "TOMs are not correct")
+    def test_peakreg_output(self):
+        img_ref = nib.load("examples/Tests/bundle_TOMs.nii.gz").get_data()
+        img_new = nib.load("examples/tractseg_output/bundle_TOMs.nii.gz").get_data()
+        images_equal = np.array_equal(img_ref, img_new)
+        self.assertTrue(images_equal, "TOMs are not correct")
