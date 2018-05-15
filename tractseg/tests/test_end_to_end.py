38889790179c1115c84735a7288d276714a384ea
wasserth <j.wasserthal@dkfz.de>
2018-05-15 16:26:14 +0200

    Bugfix

--- tractseg/tests/test_end_to_end.py
+++ tractseg/tests/test_end_to_end.py
@@ -9,7 +9,7 @@ class test_end_to_end(unittest.TestCase):
 
     def test_tractseg_output(self):
         img_ref = nib.load("examples/example_output_TractSeg.nii.gz").get_data()
-        img_new = nib.load("tractseg_output/bundle_segmentations.nii.gz").get_data()
+        img_new = nib.load("examples/tractseg_output/bundle_segmentations.nii.gz").get_data()
 
         images_equal = np.array_equal(img_ref, img_new)
 
@@ -17,7 +17,7 @@ class test_end_to_end(unittest.TestCase):
 
     # def test_peakreg_output(self):
     #     img_ref = nib.load("examples/example_output_TOM.nii.gz").get_data()
-    #     img_new = nib.load("tractseg_output/bundle_TOMs.nii.gz").get_data()
+    #     img_new = nib.load("examples/tractseg_output/bundle_TOMs.nii.gz").get_data()
     #
     #     images_equal = np.array_equal(img_ref, img_new)
     #
