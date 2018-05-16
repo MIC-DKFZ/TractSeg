36d15151f8c9b1405cdfb5145dfc9b9cf0ec2e76
wasserth <j.wasserthal@dkfz.de>
2018-05-16 12:25:15 +0200

    Add tolerance margin to TOM comparison

--- tractseg/tests/test_end_to_end.py
+++ tractseg/tests/test_end_to_end.py
@@ -14,7 +14,7 @@ class test_end_to_end(unittest.TestCase):
     #     self.assertTrue(images_equal, "Tract segmentations are not correct")
 
     def test_peakreg_output(self):
-        img_ref = nib.load("examples/Tests/bundle_TOMs.nii.gz").get_data()  # has also to be created with CPU Pytorch otherwise not same results
+        img_ref = nib.load("examples/Tests/bundle_TOMs.nii.gz").get_data()
         img_new = nib.load("examples/tractseg_output/bundle_TOMs.nii.gz").get_data()
-        images_equal = np.array_equal(img_ref, img_new)
+        images_equal = np.allclose(img_ref, img_new, rtol=1e-5, atol=1e-5)  # because of floats small tolerance margin needed
         self.assertTrue(images_equal, "TOMs are not correct")
