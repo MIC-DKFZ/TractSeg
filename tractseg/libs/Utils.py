00ac6529baa3bb7e625bb622dc97f05e994808cc
wasserth <j.wasserthal@dkfz.de>
2018-07-16 12:58:56 +0200

    Bugfix

--- tractseg/libs/Utils.py
+++ tractseg/libs/Utils.py
@@ -179,15 +179,16 @@ class Utils:
 
     @staticmethod
     def download_pretrained_weights(experiment_type, dropout_sampling=False):
-        if experiment_type == "tract_segmentation":
-            weights_path_old = os.path.join(C.TRACT_SEG_HOME, 'pretrained_weights_v1.npz')
-            weights_path = os.path.join(C.TRACT_SEG_HOME, 'pretrained_weights_tract_segmentation_v1.npz')
-            WEIGHTS_URL = "https://www.dropbox.com/s/nygr0j2zgztedh0/TractSeg_best_weights_ep448.npz?dl=1"
 
-        elif experiment_type == "tract_segmentation" and dropout_sampling:
+        if experiment_type == "tract_segmentation" and dropout_sampling:
             weights_path_old = os.path.join(C.TRACT_SEG_HOME, 'pretrained_weights_v1.npz')
             weights_path = os.path.join(C.TRACT_SEG_HOME, 'pretrained_weights_tract_segmentation_dropout_v1.npz')
             WEIGHTS_URL = "https://www.dropbox.com/s/m3ccn286uy1rrhz/TractSeg_Dropout_best_weights_ep488.npz?dl=1"
+
+        elif experiment_type == "tract_segmentation":
+            weights_path_old = os.path.join(C.TRACT_SEG_HOME, 'pretrained_weights_v1.npz')
+            weights_path = os.path.join(C.TRACT_SEG_HOME, 'pretrained_weights_tract_segmentation_v1.npz')
+            WEIGHTS_URL = "https://www.dropbox.com/s/nygr0j2zgztedh0/TractSeg_best_weights_ep448.npz?dl=1"
 
         elif experiment_type == "endings_segmentation":
             weights_path_old = os.path.join(C.TRACT_SEG_HOME, 'pretrained_weights.npz')
