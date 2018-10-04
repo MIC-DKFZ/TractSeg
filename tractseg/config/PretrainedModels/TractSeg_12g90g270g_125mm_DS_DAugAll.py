95ac7e12a9ac127f5a0dd1080a7d5274467892f3
wasserth <j.wasserthal@dkfz.de>
2018-10-04 14:24:18 +0200

    Change default nr of epochs

--- tractseg/config/PretrainedModels/TractSeg_12g90g270g_125mm_DS_DAugAll.py
+++ tractseg/config/PretrainedModels/TractSeg_12g90g270g_125mm_DS_DAugAll.py
@@ -5,6 +5,6 @@ from tractseg.config.TractSegHP import HP as TractSegHP
 class HP(TractSegHP):
     EXP_NAME = os.path.basename(__file__).split(".")[0]
 
-    NUM_EPOCHS = 500
+    NUM_EPOCHS = 250
     DATA_AUGMENTATION = True
     MODEL = "UNet_Pytorch_DeepSup"
