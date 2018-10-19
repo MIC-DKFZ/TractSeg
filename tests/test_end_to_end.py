f3c493ccbb401b778a93d3c7d8a5608dfbe4d564
wasserth <j.wasserthal@dkfz.de>
2018-10-19 09:41:28 +0200

    remove old reference bundles

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -8,9 +8,11 @@ class test_end_to_end(unittest.TestCase):
     def setUp(self):
         pass
 
-    # def test_csd_peaks(self):
-    #     #todo
-    #     pass
+    def test_csd_peaks(self):
+        img_ref = nib.load("tests/reference_files/peaks.nii.gz").get_data()
+        img_new = nib.load("examples/tractseg_output/peaks.nii.gz").get_data()
+        images_equal = np.array_equal(img_ref, img_new)
+        self.assertTrue(images_equal, "CSD peaks not correct")
 
     def test_tractseg_output(self):
         bundles = ExpUtils.get_bundle_names("All")[1:]
@@ -20,25 +22,36 @@ class test_end_to_end(unittest.TestCase):
             images_equal = np.array_equal(img_ref, img_new)
             self.assertTrue(images_equal, "Tract segmentations are not correct (bundle: " + bundle + ")")
 
-    # def test_endingsseg_output(self):
-    #     img_ref = nib.load("examples/Tests/bundle_endings.nii.gz").get_data()
-    #     img_new = nib.load("examples/tractseg_output/bundle_endings.nii.gz").get_data()
-    #     images_equal = np.array_equal(img_ref, img_new)
-    #     self.assertTrue(images_equal, "Bundle endings segmentations are not correct")
+    def test_endingsseg_output(self):
+        bundles = ExpUtils.get_bundle_names("All")[1:]
+        for bundle in bundles:
+            img_ref = nib.load("tests/reference_files/endings_segmentations/" + bundle + "_b.nii.gz").get_data()
+            img_new = nib.load("examples/tractseg_output/endings_segmentations/" + bundle + "_b.nii.gz").get_data()
+            images_equal = np.array_equal(img_ref, img_new)
+            self.assertTrue(images_equal, "Bundle endings are not correct (bundle: " + bundle + "_b)")
 
-    # def test_peakreg_output(self):
-    #     img_ref = nib.load("examples/Tests/bundle_TOMs.nii.gz").get_data()
-    #     img_new = nib.load("examples/tractseg_output/bundle_TOMs.nii.gz").get_data()
-    #     images_equal = np.allclose(img_ref, img_new, rtol=1e-5, atol=1e-5)  # because of floats small tolerance margin needed
-    #     self.assertTrue(images_equal, "TOMs are not correct")
-    #
-    # def test_FA(self):
+            img_ref = nib.load("tests/reference_files/endings_segmentations/" + bundle + "_e.nii.gz").get_data()
+            img_new = nib.load("examples/tractseg_output/endings_segmentations/" + bundle + "_e.nii.gz").get_data()
+            images_equal = np.array_equal(img_ref, img_new)
+            self.assertTrue(images_equal, "Bundle endings are not correct (bundle: " + bundle + "_e)")
+
+    def test_peakreg_output(self):
+        bundles = ExpUtils.get_bundle_names("All")[1:]
+        for bundle in bundles:
+            img_ref = nib.load("tests/reference_files/TOM/" + bundle + ".nii.gz").get_data()
+            img_new = nib.load("examples/tractseg_output/TOM/" + bundle + ".nii.gz").get_data()
+            images_equal = np.allclose(img_ref, img_new, rtol=1e-5, atol=1e-5)  # because of floats small tolerance margin needed
+            self.assertTrue(images_equal, "TOMs are not correct (bundle: " + bundle + ")")
+
+    def test_FA(self):
+        img_ref = nib.load("tests/reference_files/FA.nii.gz").get_data()
+        img_new = nib.load("examples/FA.nii.gz").get_data()
+        images_equal = np.array_equal(img_ref, img_new)
+        self.assertTrue(images_equal, "FA not correct")
+
+    def test_tractometry(self):
         #todo
-        # self.assertTrue(3 == 3, "test test")
-    #
-    # def test_tractometry(self):
-    #     #todo
-    #     pass
+        pass
 
 if __name__ == '__main__':
     unittest.main()
