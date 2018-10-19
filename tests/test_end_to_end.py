017d9e1a4d836f9b4eb44e97d3b9ebe6acdc4234
wasserth <j.wasserthal@dkfz.de>
2018-10-19 09:17:12 +0200

    Refactor folder name

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -15,7 +15,7 @@ class test_end_to_end(unittest.TestCase):
     def test_tractseg_output(self):
         bundles = ExpUtils.get_bundle_names("All")[1:]
         for bundle in bundles:
-            img_ref = nib.load("tests/Reference_files/bundle_segmentations/" + bundle + ".nii.gz").get_data()
+            img_ref = nib.load("tests/reference_files/bundle_segmentations/" + bundle + ".nii.gz").get_data()
             img_new = nib.load("examples/tractseg_output/bundle_segmentations/" + bundle + ".nii.gz").get_data()
             images_equal = np.array_equal(img_ref, img_new)
             self.assertTrue(images_equal, "Tract segmentations are not correct (bundle: " + bundle + ")")
