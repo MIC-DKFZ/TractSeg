ab867b2b52e377d75da8c67f12e77c7105e11f96
wasserth <j.wasserthal@dkfz.de>
2019-04-01 18:02:46 +0100

    Automatic nonHCP eval after training

--- tractseg/data/data_loader_training.py
+++ tractseg/data/data_loader_training.py
@@ -83,6 +83,14 @@ def load_training_data(Config, subject):
             data = load(join(C.DATA_PATH, Config.DATASET_FOLDER, subject, "90g_125mm_peaks"))
         else:
             data = load(join(C.DATA_PATH, Config.DATASET_FOLDER, subject, "12g_125mm_peaks"))
+    elif Config.FEATURES_FILENAME == "12g90g270g_BX":
+        rnd_choice = np.random.random()
+        if rnd_choice < 0.33:
+            data = load(join(C.DATA_PATH, Config.DATASET_FOLDER, subject, "270g_125mm_bedpostx_peaks_scaled"))
+        elif rnd_choice < 0.66:
+            data = load(join(C.DATA_PATH, Config.DATASET_FOLDER, subject, "90g_125mm_bedpostx_peaks_scaled"))
+        else:
+            data = load(join(C.DATA_PATH, Config.DATASET_FOLDER, subject, "12g_125mm_bedpostx_peaks_scaled"))
     elif Config.FEATURES_FILENAME == "T1_Peaks270g":
         peaks = load(join(C.DATA_PATH, Config.DATASET_FOLDER, subject, "270g_125mm_peaks"))
         t1 = load(join(C.DATA_PATH, Config.DATASET_FOLDER, subject, "T1"))
--- tractseg/data/preprocessing.py
+++ tractseg/data/preprocessing.py
@@ -31,13 +31,14 @@ from tractseg.libs import dataset_utils
 from tractseg.libs.subjects import get_all_subjects
 from tractseg.libs import exp_utils
 
-# dataset = "HCP_final"
-# DATASET_FOLDER = "HCP_for_training_COPY"  # source folder
-# DATASET_FOLDER_PREPROC = "HCP_preproc"  # target folder
+#todo: adapt
+dataset = "HCP_final"
+DATASET_FOLDER = "HCP_for_training_COPY"  # source folder
+DATASET_FOLDER_PREPROC = "HCP_preproc"  # target folder
 
-dataset = "HCP_all"
-DATASET_FOLDER = "HCP_for_training_COPY_all"  # source folder
-DATASET_FOLDER_PREPROC = "HCP_preproc_all"  # target folder
+# dataset = "HCP_all"
+# DATASET_FOLDER = "HCP_for_training_COPY_all"  # source folder
+# DATASET_FOLDER_PREPROC = "HCP_preproc_all"  # target folder
 
 def create_preprocessed_files(subject):
 
@@ -67,8 +68,12 @@ def create_preprocessed_files(subject):
     # filenames_data = ["12g_125mm_peaks", "125mm_bedpostx_peaks_scaled"]
     # filenames_seg = ["-"]
 
-    filenames_data = ["125mm_bedpostx_peaks", "125mm_bedpostx_peaks_scaled"]
-    filenames_seg = ["-"]
+    # filenames_data = ["125mm_bedpostx_peaks", "12g_125mm_bedpostx_peaks_scaled", "90g_125mm_bedpostx_peaks_scaled",
+    #                   "270g_125mm_bedpostx_peaks_scaled"]
+    # filenames_seg = ["-"]
+
+    filenames_data = ["125mm_bedpostx_peaks"]
+    filenames_seg = ["bundle_masks_autoPTX_thr001"]
 
     print("idx: {}".format(subjects.index(subject)))
     exp_utils.make_dir(join(C.DATA_PATH, DATASET_FOLDER_PREPROC, subject))
@@ -90,12 +95,12 @@ def create_preprocessed_files(subject):
             nib.save(nib.Nifti1Image(data, affine), join(C.DATA_PATH, DATASET_FOLDER_PREPROC, subject,
                                                          filename + ".nii.gz"))
 
-    # for filename in filenames_seg:
-    #     data = nib.load(join(C.NETWORK_DRIVE, DATASET_FOLDER, subject, filename + ".nii.gz")).get_data()
-    #     data, _, _, _ = dataset_utils.crop_to_nonzero(data, bbox=bbox)
-    #     # np.save(join(C.DATA_PATH, DATASET_FOLDER_PREPROC, subject, filename + ".npy"), data)
-    #     nib.save(nib.Nifti1Image(data, affine), join(C.DATA_PATH, DATASET_FOLDER_PREPROC, subject, filename +
-    #                                                  ".nii.gz"))
+    for filename in filenames_seg:
+        data = nib.load(join(C.NETWORK_DRIVE, DATASET_FOLDER, subject, filename + ".nii.gz")).get_data()
+        data, _, _, _ = dataset_utils.crop_to_nonzero(data, bbox=bbox)
+        # np.save(join(C.DATA_PATH, DATASET_FOLDER_PREPROC, subject, filename + ".npy"), data)
+        nib.save(nib.Nifti1Image(data, affine), join(C.DATA_PATH, DATASET_FOLDER_PREPROC, subject, filename +
+                                                     ".nii.gz"))
 
 
 if __name__ == "__main__":
