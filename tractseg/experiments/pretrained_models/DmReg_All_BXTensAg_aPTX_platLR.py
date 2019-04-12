b889103bfcb725e02a069a1ad39ed4687c9dcee9
wasserth <j.wasserthal@dkfz.de>
2019-04-12 14:10:37 +0100

    Add BX and aPTX to interface

---
+++ tractseg/experiments/pretrained_models/DmReg_All_BXTensAg_aPTX_platLR.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from tractseg.libs import exp_utils
+from tractseg.experiments.dm_reg import Config as DmRegConfig
+
+
+class Config(DmRegConfig):
+    EXP_NAME = os.path.basename(__file__).split(".")[0]
+
+    DATASET_FOLDER = "HCP_preproc_all"
+    NR_OF_GRADIENTS = 18
+    FEATURES_FILENAME = "32g270g_BX"
+    P_SAMP = 0.4
+
+    CLASSES = "AutoPTX_42"
+    NR_OF_CLASSES = len(exp_utils.get_bundle_names(CLASSES)[1:])
+
+    THRESHOLD = 0.001
+
+    DATASET = "HCP_all"
+
+    LR_SCHEDULE = True
+    LR_SCHEDULE_MODE = "min"
+    LR_SCHEDULE_PATIENCE = 10
+
+    NUM_EPOCHS = 200    # 130 probably also fine
