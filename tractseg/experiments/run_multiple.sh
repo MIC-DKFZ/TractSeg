c829ddcba52ca530afc59a7100efa2271985f06e
wasserth <j.wasserthal@dkfz.de>
2019-05-24 12:52:32 +0200

    Add run_multiple

---
+++ tractseg/experiments/run_multiple.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e  # stop on error
+
+~/my_bsub.sh PeaksPart1_AngL
+~/my_bsub.sh PeaksPart1_AngL_xyz
+~/my_bsub.sh PeaksPart1_w10
+~/my_bsub.sh PeaksPart1_w10_800
+~/my_bsub.sh PeaksPart1_w10_xyz
+~/my_bsub.sh PeaksPart1_w20
+~/my_bsub.sh TractSeg_cu9
