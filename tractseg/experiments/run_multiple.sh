098c39b3832f03945955034ea692680577591253
wasserth <j.wasserthal@dkfz.de>
2019-05-28 16:17:20 +0200

    Add peak reg angle base exp

--- tractseg/experiments/run_multiple.sh
+++ tractseg/experiments/run_multiple.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 set -e  # stop on error
 
-~/my_bsub.sh PeaksPart1_AngL
-~/my_bsub.sh PeaksPart1_AngL_xyz
-~/my_bsub.sh PeaksPart1_w10
-~/my_bsub.sh PeaksPart1_w10_800
-~/my_bsub.sh PeaksPart1_w10_xyz
-~/my_bsub.sh PeaksPart1_w20
-~/my_bsub.sh TractSeg_cu9
+#~/my_bsub.sh PeaksPart1_AngL
+#~/my_bsub.sh PeaksPart1_AngL_xyz
+#~/my_bsub.sh PeaksPart1_w10
+#~/my_bsub.sh PeaksPart1_w10_800
+#~/my_bsub.sh PeaksPart1_w10_xyz
+#~/my_bsub.sh PeaksPart1_w20
+#~/my_bsub.sh TractSeg_cu9
+
+~/my_bsub.sh PeaksPart1_AngL_fold0
+~/my_bsub.sh PeaksPart1_AngL_fold1
+~/my_bsub.sh PeaksPart1_AngL_fold2
+~/my_bsub.sh PeaksPart1_AngL_fold3
+~/my_bsub.sh PeaksPart1_AngL_fold4
+~/my_bsub.sh PeaksPart1_AngL_fold5
+~/my_bsub.sh PeaksPart2_AngL_fold0
+~/my_bsub.sh PeaksPart2_AngL_fold1
+~/my_bsub.sh PeaksPart2_AngL_fold2
+~/my_bsub.sh PeaksPart2_AngL_fold3
+~/my_bsub.sh PeaksPart2_AngL_fold4
+~/my_bsub.sh PeaksPart2_AngL_fold5
+~/my_bsub.sh PeaksPart3_AngL_fold0
+~/my_bsub.sh PeaksPart3_AngL_fold1
+~/my_bsub.sh PeaksPart3_AngL_fold2
+~/my_bsub.sh PeaksPart3_AngL_fold3
+~/my_bsub.sh PeaksPart3_AngL_fold4
+~/my_bsub.sh PeaksPart3_AngL_fold5
+~/my_bsub.sh PeaksPart4_AngL_fold0
+~/my_bsub.sh PeaksPart4_AngL_fold1
+~/my_bsub.sh PeaksPart4_AngL_fold2
+~/my_bsub.sh PeaksPart4_AngL_fold3
+~/my_bsub.sh PeaksPart4_AngL_fold4
+~/my_bsub.sh PeaksPart4_AngL_fold5
