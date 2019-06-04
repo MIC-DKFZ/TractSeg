c615b4ecad9b27d7680364797781b7b2aaeb614a
wasserth <j.wasserthal@dkfz.de>
2019-06-04 18:58:05 +0200

    make tqdm work on ipython notebook

--- tractseg/libs/trainer.py
+++ tractseg/libs/trainer.py
@@ -24,13 +24,19 @@ import time
 import pickle
 import socket
 import datetime
-from tqdm import tqdm
 import numpy as np
 
 from tractseg.libs import exp_utils
 from tractseg.libs import metric_utils
 from tractseg.libs import dataset_utils
 from tractseg.libs import plot_utils
+
+#todo: adapt
+try:
+    get_ipython()
+    from tqdm import tqdm_notebook as tqdm
+except:
+    from tqdm import tqdm
 
 
 def train_model(Config, model, data_loader):
