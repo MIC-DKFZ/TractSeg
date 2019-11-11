6081b10dc8ca1934e1a555f862186d07c4b75985
wasserth <j.wasserthal@dkfz.de>
2019-11-11 17:30:50 +0100

    Add cutting plane tractometry code (2)

--- tractseg/libs/tractometry.py
+++ tractseg/libs/tractometry.py
@@ -12,6 +12,9 @@ from dipy.segment.clustering import QuickBundles
 from dipy.segment.metric import AveragePointwiseEuclideanMetric
 from scipy.spatial import cKDTree
 from dipy.tracking.streamline import Streamlines
+from dipy.tracking.streamline import transform_streamlines
+from dipy.tracking.streamline import values_from_volume
+import dipy.stats.analysis as dsa
 
 from tractseg.libs import fiber_utils
 
@@ -53,6 +56,8 @@ def evaluate_along_streamlines(scalar_img, streamlines, beginnings, nr_points, d
     # - AFQ:                      ?s (test),     ?s (all),      85s  (test 4 bundles, 100 points)
     # => AFQ a lot slower than others
 
+    streamlines = list(transform_streamlines(streamlines, np.linalg.inv(affine)))
+
     for i in range(dilate):
         beginnings = binary_dilation(beginnings)
     beginnings = beginnings.astype(np.uint8)
@@ -62,122 +67,141 @@ def evaluate_along_streamlines(scalar_img, streamlines, beginnings, nr_points, d
         best_orig_peaks = fiber_utils.get_best_original_peaks(predicted_peaks, scalar_img, peak_len_thr=0.00001)
         scalar_img = np.linalg.norm(best_orig_peaks, axis=-1)
 
-
-    ### Sampling ###
-
-    #################################### Sampling map_coordinates #####################
-    values = map_coordinates(scalar_img, np.array(streamlines).T, order=1)
-    ###################################################################################
-
-    #################################### Sampling values_from_volume ##################
-    # streamlines = list(transform_streamlines(streamlines, affine))  # this has to be here; not remove previous one
-    # values = np.array(values_from_volume(scalar_img, streamlines, affine=affine)).T
-    ###################################################################################
+    #todo important: change
+    algorithm = "cutting_plane"  # equal_dist | distance_map | cutting_plane | afq
 
 
-    ### Aggregation ###
-
-    #################################### Aggregating by MEAN ##########################
-    # values_mean = np.array(values).mean(axis=1)
-    # values_std = np.array(values).std(axis=1)
-    # return values_mean, values_std
-    ###################################################################################
-
-    #################################### Aggregating by cKDTree #######################
-    # metric = AveragePointwiseEuclideanMetric()
-    # qb = QuickBundles(threshold=100., metric=metric)
-    # clusters = qb.cluster(streamlines)
-    # centroids = Streamlines(clusters.centroids)
-    # if len(centroids) > 1:
-    #     print("WARNING: number clusters > 1 ({})".format(len(centroids)))
-    # _, segment_idxs = cKDTree(centroids.data, 1, copy_data=True).query(streamlines, k=1)  # (2000, 20)
-    #
-    # values_t = np.array(values).T  # (2000, 20)
-    #
-    # # If we want to take weighted mean like in AFQ:
-    # # weights = dsa.gaussian_weights(Streamlines(streamlines))
-    # # values_t = weights * values_t
-    # # return np.sum(values_t, 0), None
-    #
-    # results_dict = defaultdict(list)
-    # for idx, sl in enumerate(values_t):
-    #     for jdx, seg in enumerate(sl):
-    #         results_dict[segment_idxs[idx, jdx]].append(seg)
+    if algorithm == "equal_dist":
+        ### Sampling ###
+        streamlines = fiber_utils.resample_fibers(streamlines, nb_points=nr_points)
+        values = map_coordinates(scalar_img, np.array(streamlines).T, order=1)
+        ### Aggregation ###
+        values_mean = np.array(values).mean(axis=1)
+        values_std = np.array(values).std(axis=1)
+        return values_mean, values_std
 
 
-    #####################################################################################
+    if algorithm == "distance_map":  # cKDTree
 
-    # Cutting Plane approach
+        ### Sampling ###
+        streamlines = fiber_utils.resample_fibers(streamlines, nb_points=nr_points)
+        values = map_coordinates(scalar_img, np.array(streamlines).T, order=1)
 
-    metric = AveragePointwiseEuclideanMetric()
-    qb = QuickBundles(threshold=100., metric=metric)
-    clusters = qb.cluster(streamlines)
-    centroid = Streamlines(clusters.centroids)[0]
+        ### Aggregating by cKDTree approach ###
+        metric = AveragePointwiseEuclideanMetric()
+        qb = QuickBundles(threshold=100., metric=metric)
+        clusters = qb.cluster(streamlines)
+        centroids = Streamlines(clusters.centroids)
+        if len(centroids) > 1:
+            print("WARNING: number clusters > 1 ({})".format(len(centroids)))
+        _, segment_idxs = cKDTree(centroids.data, 1, copy_data=True).query(streamlines, k=1)  # (2000, 20)
 
-    streamlines = fiber_utils.resample_to_same_distance(streamlines, max_nr_points=nr_points)
+        values_t = np.array(values).T  # (2000, 20)
 
-    # index of the middle cluster
-    middle_idx = int(nr_points / 2)
-    middle_point = centroid[middle_idx]
-    segment_idxs = fiber_utils.get_idxs_of_closest_points(streamlines, middle_point)
+        # If we want to take weighted mean like in AFQ:
+        # weights = dsa.gaussian_weights(Streamlines(streamlines))
+        # values_t = weights * values_t
+        # return np.sum(values_t, 0), None
+
+        results_dict = defaultdict(list)
+        for idx, sl in enumerate(values_t):
+            for jdx, seg in enumerate(sl):
+                results_dict[segment_idxs[idx, jdx]].append(seg)
+
+        if len(results_dict.keys()) < nr_points:
+            print("WARNING: found less than required points. Filling up with centroid values.")
+            centroid_values = map_coordinates(scalar_img, np.array([centroids[0]]).T, order=1)
+            for i in range(nr_points):
+                if len(results_dict[i]) == 0:
+                    results_dict[i].append(np.array(centroid_values).T[0, i])
+
+        results_mean = []
+        results_std = []
+        for key in sorted(results_dict.keys()):
+            value = results_dict[key]
+            if len(value) > 0:
+                results_mean.append(np.array(value).mean())
+                results_std.append(np.array(value).std())
+            else:
+                print("WARNING: empty segment")
+                results_mean.append(0)
+                results_std.append(0)
+        return results_mean, results_std
 
 
-    #todo: adapt
-    # Align along the middle and assign indices
-    segment_idxs_eqlen = []
-    for idx, sl in enumerate(streamlines):
-        sl_middle_pos = segment_idxs[idx]
-        before_elems = sl_middle_pos
-        after_elems = len(sl) - sl_middle_pos
+    elif algorithm == "cutting_plane":
+
+        ### Sampling ###
+        streamlines = fiber_utils.resample_to_same_distance(streamlines, max_nr_points=nr_points)
+        # map_coordinates does not allow streamlines with different lengths -> use values_from_volume
+        values = np.array(values_from_volume(scalar_img, streamlines, affine=np.eye(4))).T
+
+        ### Aggregating by Cutting Plane approach ###
+        streamlines_resamp = fiber_utils.resample_fibers(streamlines, nb_points=nr_points)  # mean over bundles
+        metric = AveragePointwiseEuclideanMetric()
+        qb = QuickBundles(threshold=100., metric=metric)
+        clusters = qb.cluster(streamlines_resamp)
+        centroids = Streamlines(clusters.centroids)
+
+        # index of the middle cluster
+        middle_idx = int(nr_points / 2)
+        middle_point = centroids[0][middle_idx]
+        segment_idxs = fiber_utils.get_idxs_of_closest_points(streamlines, middle_point)
+
+        # Align along the middle and assign indices
+        segment_idxs_eqlen = []
         base_idx = 1000  # use higher index to avoid negative numbers for area below middle
-        r = range((base_idx - before_elems), (base_idx + after_elems))
-        segment_idxs_eqlen.append(r)
-    segment_idxs = segment_idxs_eqlen
+        for idx, sl in enumerate(streamlines):
+            sl_middle_pos = segment_idxs[idx]
+            before_elems = sl_middle_pos
+            after_elems = len(sl) - sl_middle_pos
+            r = range((base_idx - before_elems), (base_idx + after_elems))
+            segment_idxs_eqlen.append(r)
+        segment_idxs = segment_idxs_eqlen
 
-    # todo: adapt
-    values_t = np.array(values).T  # (2000, 20)
-    results_dict = defaultdict(list)
-    for idx, sl in enumerate(values_t):
-        for jdx, seg in enumerate(sl):
-            results_dict[segment_idxs[idx, jdx]].append(seg)
+        values_t = values  # (2000, X)
 
+        max_idx = base_idx + int(nr_points / 2)
+        min_idx = base_idx - int(nr_points / 2)
 
-
-    ###############
-
-    if len(results_dict.keys()) < nr_points:
-        print("WARNING: found less than required points. Filling up with centroid values.")
-        centroid_values = map_coordinates(scalar_img, np.array([centroids[0]]).T, order=1)
-        for i in range(nr_points):
-            if len(results_dict[i]) == 0:
-                results_dict[i].append(np.array(centroid_values).T[0, i])
-
-    results_mean = []
-    results_std = []
-    for key in sorted(results_dict.keys()):
-        value = results_dict[key]
-        if len(value) > 0:
-            results_mean.append(np.array(value).mean())
-            results_std.append(np.array(value).std())
-        else:
-            print("WARNING: empty segment")
-            results_mean.append(0)
-            results_std.append(0)
-
-    return results_mean, results_std
-    ###################################################################################
+        results_dict = defaultdict(list)
+        for idx, sl in enumerate(values_t):
+            for jdx, seg in enumerate(sl):
+                current_idx = segment_idxs[idx][jdx]
+                if current_idx >= min_idx and current_idx < max_idx:
+                    results_dict[current_idx].append(seg)
 
 
-    #################################### Aggregating by cutting plane #######################
+        if len(results_dict.keys()) < nr_points:
+            print("WARNING: found less than required points. Filling up with centroid values.")
+            centroid_sl = [centroids[0]]
+            centroid_sl = np.array(centroid_sl).T
+            centroid_values = map_coordinates(scalar_img, centroid_sl, order=1)
+            for idx, seg_idx in enumerate(range(min_idx, max_idx)):
+                if len(results_dict[seg_idx]) == 0:
+                    results_dict[seg_idx].append(np.array(centroid_values).T[0, idx])
 
-    ###################################################################################
+
+        results_mean = []
+        results_std = []
+        for key in sorted(results_dict.keys()):
+            value = results_dict[key]
+            if len(value) > 0:
+                results_mean.append(np.array(value).mean())
+                results_std.append(np.array(value).std())
+            else:
+                print("WARNING: empty segment")
+                results_mean.append(0)
+                results_std.append(0)
+        return results_mean, results_std
 
 
-    #################################### AFQ (sampling + aggregation ##################
-    # streamlines = list(transform_streamlines(streamlines, affine))  # this has to be here; not remove previous one
-    # streamlines = Streamlines(streamlines)
-    # weights = dsa.gaussian_weights(streamlines)
-    # results_mean = dsa.afq_profile(scalar_img, streamlines, affine=affine, weights=weights)
-    # results_std = None
-    # return results_mean, results_std
-    ###################################################################################
+    elif algorithm == "afq":
+        ### AFQ (sampling + aggregation ###
+        streamlines = fiber_utils.resample_fibers(streamlines, nb_points=nr_points)
+        streamlines = list(transform_streamlines(streamlines, affine))  # this has to be here; not remove previous one
+        streamlines = Streamlines(streamlines)
+        weights = dsa.gaussian_weights(streamlines)
+        results_mean = dsa.afq_profile(scalar_img, streamlines, affine=affine, weights=weights)
+        results_std = None
+        return results_mean, results_std
